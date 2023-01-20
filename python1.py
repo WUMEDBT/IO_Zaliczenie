29a3e8a3080b8e10348e3dba4aa7e91946eb4e49
Bartosz Trupiszewski <btrupiszewski.wumed@gmail.com>
2023-01-20 11:12:05 +0100

    2 Pliki projektu py

--- python1.py
+++ python1.py
@@ -3,9 +3,3 @@ text = input()
 print("Ilość słow: ", len(text.split(" ")))
 print("Liczba słów: ", len(text.replace(" ", " ")))
 
-c = 0
-for char in text:
-	if char.isalnum():
-	c += 1
-	
-print("Znaki alfanumeryczne: ", c)
