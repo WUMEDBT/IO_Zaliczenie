6559ca18e0b768f9c061358fc0f5ace44a3de1d8
Bartosz <btrupiszewski.wumed@gmail.com>
2023-01-21 09:47:26 +0100

    Dodanie 2 plików PY do GIta

---
+++ proj1.py
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+text = input()
+
+print("Ilość słow: ", len(text.split(" ")))
+print("Liczba słów: ", len(text.replace(" ", " ")))
+
+=======
+text = input()
+
+print("Ilość słow: ", len(text.split(" ")))
+print("Liczba słów: ", len(text.replace(" ", " ")))
+
+>>>>>>> c650fecdd8c123f08567c4fcdaa6bc71a89afa4f
