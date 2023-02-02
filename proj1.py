01016cf90622c9867b46ccddc52191c4eeee05c7
Michal Juszczuk <juszczuk@protonmail.com>
2023-02-02 10:33:09 +0100

    Modyfikacja pliku proj1.py

--- proj1.py
+++ proj1.py
@@ -10,4 +10,6 @@ text = input()
 print("Ilość słow: ", len(text.split(" ")))
 print("Liczba słów: ", len(text.replace(" ", " ")))
 
+print ('Dziękujemy za uwagę, życzymy dobrego dnia.')
+
 >>>>>>> c650fecdd8c123f08567c4fcdaa6bc71a89afa4f
